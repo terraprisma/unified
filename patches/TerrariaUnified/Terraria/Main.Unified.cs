7b79ee49c19bffeb249f521a94ac23e2ab2b54d5
tomat <xxlennygamerxx@gmail.com>
2023-11-25 21:32:18 -0800

    Initial porting of repository files.

---
+++ patches/TerrariaUnified/Terraria/Main.Unified.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Terraria.DataStructures;
+
+namespace Terraria;
+
+partial class Main
+{
+	internal static string unifiedVersion = "0.1.0";
+
+	internal static List<TitleLinkButton> unifiedLinks = new List<TitleLinkButton>();
+
+	internal const bool unifiedBranding = true;
+}
